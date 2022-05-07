5fa212af20ca0d26d6ab82488381c87616c6e52d
Jude Wilson <jude@alecw.net>
2022-05-07 18:41:01 -0400

    fixed memory overflow errors

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ let year = date_ob.getFullYear();
 let today = year + "-" + month + "-" + date
 
 console.log(`• Making "${today}" in "/data" Directory`)
-exec(`mkdir ${__dirname}/data/${today}`)
+exec(`mkdir ${__dirname}/data/${today}`, {maxBuffer: 1024 * 2000})
 	.then(function(result) {
 		var stdout = result.stdout;
 		var stderr = result.stderr;
 		console.log(stdout);
 		console.log(stderr);
-		exec(`mkdir ${__dirname}/data/${today}/mbta`)
-		exec(`mkdir ${__dirname}/data/${today}/mta`)
+		exec(`mkdir ${__dirname}/data/${today}/mbta`, {maxBuffer: 1024 * 2000})
+		exec(`mkdir ${__dirname}/data/${today}/mta`, {maxBuffer: 1024 * 2000})
 
 	})
 	.catch(function(err) {
@@ -26,37 +26,37 @@ exec(`mkdir ${__dirname}/data/${today}`)
 	});
 
 console.log(`• Importing GTFS data for MBTA in Massachusetts`)
-exec(`gtfs-import --configPath ${__dirname}/config/ma/mbta.json`)
+exec(`gtfs-import --configPath ${__dirname}/config/ma/mbta.json`, {maxBuffer: 1024 * 2000})
 	.then(function(result) {
 		var stdout = result.stdout;
 		var stderr = result.stderr;
 		console.log(stdout);
 		console.log(stderr);
 		console.log(`• Formatting GTFS data for MBTA in Massachusetts`)
-		exec(`node config/ma/mbta.js`)
+		exec(`node config/ma/mbta.js`, {maxBuffer: 1024 * 2000})
 	})
 	.catch(function(err) {
 		console.log('---------------------------------------------------------');
 		console.error('ERROR: ', err);
 		console.log('---------------------------------------------------------');
 		console.log(`• Formatting GTFS data for MBTA in Massachusetts`)
-		exec(`node config/ma/mbta.js`)
+		exec(`node config/ma/mbta.js`, {maxBuffer: 1024 * 2000})
 	});
 
 console.log(`• Importing GTFS data for MTA in New York`)
-exec(`gtfs-import --configPath ${__dirname}/config/ny/mta.json`)
+exec(`gtfs-import --configPath ${__dirname}/config/ny/mta.json`, {maxBuffer: 1024 * 2000})
 	.then(function(result) {
 		var stdout = result.stdout;
 		var stderr = result.stderr;
 		console.log(stdout);
 		console.log(stderr);
 		console.log(`• Formatting GTFS data for MTA in New York`)
-		exec(`node config/ny/mta.js`)
+		exec(`node config/ny/mta.js`, {maxBuffer: 1024 * 2000})
 	})
 	.catch(function(err) {
 		console.log('---------------------------------------------------------');
 		console.error('ERROR: ', err);
 		console.log('---------------------------------------------------------');
 		console.log(`• Formatting GTFS data for MTA in New York`)
-		exec(`node config/ny/mta.js`)
+		exec(`node config/ny/mta.js`, {maxBuffer: 1024 * 2000})
 	});
